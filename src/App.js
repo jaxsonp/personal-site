7cc648682251314395b42786bf786726f50c868f
Jaxson Pahukula <jaxpahu@gmail.com>
2024-03-18 18:42:55 -0400

    create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+		<h1>Jaxson Pahukula</h1>
+		<p>welcome to my portfolio</p>
+    </div>
+  );
+}
+
+export default App;
