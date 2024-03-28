ddbc351b007519932d02d3f1d44245c802e71216
Jaxson Pahukula <jaxpahu@gmail.com>
2024-03-28 16:00:10 -0400

    add wip message

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.css';
 </script>
 
+<header class="alert">
+	<p>🚧 This page is currently a work on progress 🚧</p>
+</header>
 <div class="background">
 	<slot />
 </div>
@@ -9,11 +12,22 @@
 <style>
 	.background {
 		background: rgb(105, 120, 87);
-		background: linear-gradient(0deg, rgba(105, 120, 87, 1) 0%, rgba(173, 216, 230, 1) 75%);
-		position: absolute;
-		top: 0;
-		left: 0;
+		background: linear-gradient(0deg, rgba(105, 120, 87, 1) 0%, rgba(173, 216, 230, 1) 66%);
 		width: 100vw;
 		min-height: 100vh;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.alert {
+		background: #ff4d4d;
+		display: flex;
+		justify-content: center;
+		padding: 0.5rem;
+	}
+	.alert > * {
+		margin: 0;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Jaxson Pahukula</h1>
-<p>welcome to my website :D</p>
+<div class="main-banner">
+	<h1>Jaxson Pahukula</h1>
+	<p>welcome to my website :D</p>
+</div>
+
+<style>
+	.main-banner {
+		width: 66vw;
+		min-width: 1000px;
+		height: 100vh;
+		margin-top: 4rem;
+
+		background: #000000;
+		opacity: 0.8;
+		/* backdrop-filter: blur(10px); */
+	}
+</style>
