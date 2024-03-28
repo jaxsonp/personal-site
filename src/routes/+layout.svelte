0f1add75f046ee2f9f972c4eea428d2c94c76a5f
Jaxson Pahukula <jaxpahu@gmail.com>
2024-03-28 01:45:54 -0400

    background placeholder

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="background">
+	<slot />
+</div>
+
+<style>
+	.background {
+		background: rgb(105, 120, 87);
+		background: linear-gradient(0deg, rgba(105, 120, 87, 1) 0%, rgba(173, 216, 230, 1) 75%);
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		min-height: 100vh;
+	}
+</style>
