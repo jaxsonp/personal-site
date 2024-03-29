a40a8ef8945f1adb3149b33b5c8310ec2a016026
Jaxson Pahukula <jaxpahu@gmail.com>
2024-03-28 22:15:50 -0400

    fix background canvas position

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,27 +2,48 @@
 	import '../app.css';
 </script>
 
-<header class="alert">
-	<p>🚧 This page is currently a work on progress 🚧</p>
-</header>
-<div class="background">
-	<slot />
+<div>
+	<div class="background"></div>
+	<!-- <header class="alert">
+		<p>🚧 This page is currently a work on progress 🚧</p>
+	</header> -->
+	<div class="content">
+		<slot />
+	</div>
+	<footer class="page-footer">
+		<p>Placeholder footer</p>
+	</footer>
 </div>
 
 <style>
 	.background {
 		background: rgb(105, 120, 87);
 		background: linear-gradient(0deg, rgba(105, 120, 87, 1) 0%, rgba(173, 216, 230, 1) 66%);
+		position: fixed;
+		top: 0;
+		left: 0;
 		width: 100vw;
-		min-height: 100vh;
+		height: 100vh;
+	}
 
+	.content {
 		display: flex;
 		justify-content: center;
 		align-items: center;
 	}
 
+	.page-footer {
+		background-color: black;
+		position: static;
+		opacity: 0.99;
+
+		padding: 0.5rem;
+	}
+
 	.alert {
 		background: #ff4d4d;
+		opacity: 0.75;
+
 		display: flex;
 		justify-content: center;
 		padding: 0.5rem;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,14 @@
 <div class="main-banner">
 	<h1>Jaxson Pahukula</h1>
 	<p>welcome to my website :D</p>
+	<p style="white-space: pre-line;">{'test\n'.repeat(50)}</p>
 </div>
 
 <style>
 	.main-banner {
 		width: 66vw;
 		min-width: 1000px;
-		height: 100vh;
+		min-height: 100vh - 4rem;
 		margin-top: 4rem;
 
 		background: #000000;
