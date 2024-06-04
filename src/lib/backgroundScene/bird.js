0ff1edd7ad4d871d810546c0ee59d1cc8b76590c
Jaxson Pahukula <jaxpahu@gmail.com>
2024-06-03 15:49:16 -1000

    add gradient background

--- src/lib/backgroundScene/bird.js
+++ src/lib/backgroundScene/bird.js
@@ -1,32 +1,22 @@
 export default class Bird {
 	static speed = 1;
 	static size = 20.0;
-	x: number;
-	y: number;
+	x = 0.0;
+	y = 0.0;
 
-	constructor(y: number) {
+	constructor(y) {
 		this.x = -Bird.size;
 		this.y = y;
 	}
 
-	moveAndDraw(ctx: CanvasRenderingContext2D, skyFill: CanvasGradient): void {
+	moveAndDraw(ctx, skyFill) {
 		ctx.fillStyle = skyFill;
 		ctx.fillRect(this.x, this.y, Bird.size, Bird.size);
 
 		this.x += Bird.speed;
 
 		ctx.fillStyle = 'black';
-		/*ctx.beginPath();
-		ctx.ellipse(
-			this.x + Bird.size / 2,
-			this.y + Bird.size / 2,
-			Bird.size,
-			Bird.size,
-			0,
-			0,
-			2 * Math.PI
-		);
-		ctx.stroke();*/
+
 		ctx.fillRect(this.x, this.y, Bird.size, Bird.size);
 	}
 }
