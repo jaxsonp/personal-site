0f72bcec47c347215e9c23f9e53fabf024909b48
Jaxson Pahukula <jaxpahu@gmail.com>
2024-08-20 21:07:08 -0400

    css figured out

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,9 +1,9 @@
-export default function SectionHeader() {
+import React from "react";
+
+export default function SectionHeader({ children }: { children: React.ReactNode }) {
 	return (
 		<div className="flex items-center mt-4">
-			<h1>
-				<slot />
-			</h1>
+			<h1>{children}</h1>
 			<div className="grow m-8">
 				<hr className="border-blue" />
 			</div>
